49fd0c260f62eff6f0c3dbeb610c1ecbdbd8bf16
yesthis1please <yesthis1please@gmail.com>
2022-09-13 21:51:30 +0900

    Added toggle function to the <p> in Tour.js

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = ({id,image,info,price,name}) => {
+const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
 
   return (
     <article className="single-tour">
@@ -11,7 +12,10 @@ const Tour = ({id,image,info,price,name}) => {
           <h4 className="tour-price">${price}</h4>
         </div>
         <p>
-          {info}
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show less" : "Read more"}
+          </button>
         </p>
         <button className="delete-btn">Not interested</button>
       </footer>
