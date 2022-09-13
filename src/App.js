f387a80c50d129d9374f6a11fac03a03b9efd3db
yesthis1please <yesthis1please@gmail.com>
2022-09-13 21:02:03 +0900

    Fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async() =>{
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
+  }
+
+  useEffect(()=>{
+    fetchTours();
+  },[])
+  if(loading){
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App
